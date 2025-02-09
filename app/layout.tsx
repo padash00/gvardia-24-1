a5d717651eacbca7de30ed90f8072d71ab32f21f
padash00 <76392786+padash00@users.noreply.github.com>
2025-02-09 05:00:55 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Аренда помещении',
   description: 'Created with v0',
 }
 
